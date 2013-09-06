e19b6ccc376a792f93db55418eac86ae603feb27
Roland Wittmann <wittmanr@in.tum.de>
2013-09-06 19:41:24 +0200

    improved test example

--- src/peanoclaw/native/main.cpp
+++ src/peanoclaw/native/main.cpp
@@ -29,7 +29,7 @@
 #if defined(SWE)
 class BreakingDam_SWEKernelScenario : public peanoclaw::native::SWEKernelScenario {
     public:
-        BreakingDam_SWEKernelScenario() {}
+        BreakingDam_SWEKernelScenario(int subfactor, int meshwidth) : _subfactor(subfactor), _meshwidth(meshwidth) {}
         ~BreakingDam_SWEKernelScenario() {}
 
         virtual void initializePatch(peanoclaw::Patch& patch) {
@@ -38,7 +38,7 @@ class BreakingDam_SWEKernelScenario : public peanoclaw::native::SWEKernelScenari
             double y0=0.5;
             
             // Riemann states of the dam break problem
-            double radDam = 0.1;
+            double radDam = 0.25;
             double hl = 2.;
             double ul = 0.;
             double vl = 0.;
@@ -101,15 +101,24 @@ class BreakingDam_SWEKernelScenario : public peanoclaw::native::SWEKernelScenari
           
             double demandedMeshWidth = 0;
             if (max_gradient > 0.05) {
-                demandedMeshWidth = 1.0/243;
-                //demandedMeshWidth = 1.0/81;
+                //demandedMeshWidth = 1.0/243;
+                //demandedMeshWidth = 1.0/(6.0*9.0);
+                demandedMeshWidth = 1.0/(_subfactor*_meshwidth);
             } else {
-                demandedMeshWidth = 1.0/243;
-                //demandedMeshWidth = 1.0/81;
+                //demandedMeshWidth = 1.0/243;
+                demandedMeshWidth = 1.0/(_subfactor*_meshwidth);
             }
 
             return demandedMeshWidth;
         }
+
+        void setMeshwidth(int meshwidth) {
+            _meshwidth = meshwidth;
+        }
+
+    private:
+        int _subfactor;
+        int _meshwidth;
 };
 #endif
 
@@ -162,6 +171,7 @@ int main(int argc, char **argv) {
   tarch::logging::CommandLineLogger::getInstance().clearFilterList();
   tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "", true ) );
   tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "info", true ) );
+
   tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "debug", true ) );
 //  tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "trace", true ) );
 
@@ -170,6 +180,8 @@ int main(int argc, char **argv) {
 //  tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "debug", -1, "peanoclaw::mappings::Remesh::destroyVertex", false ) );
 //  tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "debug", -1, "peanoclaw::mappings::Remesh::endIteration", false ) );
 //  tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "debug", -1, "peanoclaw::mappings::Remesh::touchVertex", false ) );
+ 
+  tarch::logging::CommandLineLogger::getInstance().addFilterListEntry( ::tarch::logging::CommandLineLogger::FilterListEntry( "info", -1, "peanoclaw::statistics::SubgridStatistics::logLevelStatistics", true ) );
 
   //tarch::logging::CommandLineLogger::getInstance().setLogFormat( ... please consult source code documentation );
 
@@ -188,84 +200,108 @@ int main(int argc, char **argv) {
   peanoclaw::NumericsFactory numericsFactory;
 
 #if defined(SWE)
-  BreakingDam_SWEKernelScenario scenario;
-  peanoclaw::Numerics* numerics = numericsFactory.createSWENumerics(scenario);
+  for (int subfactor=3; subfactor <= 3; subfactor+=3) {
+      BreakingDam_SWEKernelScenario scenario(subfactor, 2187); // 2187 equals 5e6 cells
+      peanoclaw::Numerics* numerics = numericsFactory.createSWENumerics(scenario);
 
-  _configuration = new peanoclaw::configurations::PeanoClawConfigurationForSpacetreeGrid;
-  // assertion1(_configuration->isValid(), _configuration);
+      _configuration = new peanoclaw::configurations::PeanoClawConfigurationForSpacetreeGrid;
+      // assertion1(_configuration->isValid(), _configuration);
 
-  //Construct parameters
-  tarch::la::Vector<DIMENSIONS, double> domainOffset(0);
-  tarch::la::Vector<DIMENSIONS, double> domainSize(1.0);
-  tarch::la::Vector<DIMENSIONS, double> initialMinimalMeshWidth(0.05);
-  tarch::la::Vector<DIMENSIONS, int> subdivisionFactor(36);
-  int ghostlayerWidth = 1;
-  int unknownsPerSubcell = 3;
-  int auxiliarFieldsPerSubcell = 0;
-  int initialTimestepSize = 0.1;
-  bool useDimensionalSplittingOptimization = true;
+      //Construct parameters
+      tarch::la::Vector<DIMENSIONS, double> domainOffset(0);
+      tarch::la::Vector<DIMENSIONS, double> domainSize(1.0);
+      tarch::la::Vector<DIMENSIONS, int> subdivisionFactor(subfactor);
+      tarch::la::Vector<DIMENSIONS, double> initialMinimalMeshWidth(0.1); // TODO: was 0.1/subfactor
+      int ghostlayerWidth = 1;
+      int unknownsPerSubcell = 3;
+      int auxiliarFieldsPerSubcell = 0;
+      int initialTimestepSize = 0.1;
+      bool useDimensionalSplittingOptimization = true;
 
-  //Check parameters
-  assertion1(tarch::la::greater(domainSize(0), 0.0) && tarch::la::greater(domainSize(1), 0.0), domainSize);
-  if(initialMinimalMeshWidth(0) > domainSize(0) || initialMinimalMeshWidth(1) > domainSize(1)) {
-    logError("main(...)", "Domainsize or initialMinimalMeshWidth not set properly.");
-  }
-  if(tarch::la::oneGreater(tarch::la::Vector<DIMENSIONS, int>(1), subdivisionFactor(0)) ) {
-    logError("main(...)", "subdivisionFactor not set properly.");
-  }
- 
-  //Create runner
- 
- 
-  peanoclaw::runners::PeanoClawLibraryRunner* runner
-    = new peanoclaw::runners::PeanoClawLibraryRunner(
-    *_configuration,
-    *numerics,
-    domainOffset,
-    domainSize,
-    initialMinimalMeshWidth,
-    subdivisionFactor,
-    ghostlayerWidth,
-    unknownsPerSubcell,
-    auxiliarFieldsPerSubcell,
-    initialTimestepSize,
-    useDimensionalSplittingOptimization
-  );
+      //Check parameters
+      assertion1(tarch::la::greater(domainSize(0), 0.0) && tarch::la::greater(domainSize(1), 0.0), domainSize);
+      if(initialMinimalMeshWidth(0) > domainSize(0) || initialMinimalMeshWidth(1) > domainSize(1)) {
+        logError("main(...)", "Domainsize or initialMinimalMeshWidth not set properly.");
+      }
+      if(tarch::la::oneGreater(tarch::la::Vector<DIMENSIONS, int>(1), subdivisionFactor(0)) ) {
+        logError("main(...)", "subdivisionFactor not set properly.");
+      }
+     
+      //Create runner
+     
+     
+      peanoclaw::runners::PeanoClawLibraryRunner* runner
+        = new peanoclaw::runners::PeanoClawLibraryRunner(
+        *_configuration,
+        *numerics,
+        domainOffset,
+        domainSize,
+        initialMinimalMeshWidth,
+        subdivisionFactor,
+        ghostlayerWidth,
+        unknownsPerSubcell,
+        auxiliarFieldsPerSubcell,
+        initialTimestepSize,
+        useDimensionalSplittingOptimization
+      );
 
 #if defined(Parallel) 
-  std::cout << tarch::parallel::Node::getInstance().getRank() << ": peano instance created" << std::endl;
+      std::cout << tarch::parallel::Node::getInstance().getRank() << ": peano instance created" << std::endl;
 #endif
 
-  assertion(runner != 0);
- 
-  // run experiment
-  double timestep = 0.001;
-  double endtime = 0.5;
+      assertion(runner != 0);
+     
+      // run experiment
+      double timestep = 0.000001;
+      double endtime = 0.001;
+      double current_time = 0.0;
 #if defined(Parallel)
-  if (tarch::parallel::Node::getInstance().isGlobalMaster()) {
+      if (tarch::parallel::Node::getInstance().isGlobalMaster()) {
 #endif
 
-      double start_meas_time = MPI_Wtime();
+          double start_meas_time = MPI_Wtime();
 
-      for (double time=0.0; time < endtime; time+=timestep) {
-        runner->evolveToTime(time);
-        //runner->gatherCurrentSolution();
-        std::cout << "time " << time << " done " << std::endl;
+#if 0
+          int target_meshwidth=81;
+          for (int active_meshwidth=81; active_meshwidth <= target_meshwidth; active_meshwidth*=3) {
+            std::cout << "switching to new mesh width: " << active_meshwidth << std::endl;
+            //scenario.setMeshwidth(active_meshwidth);
+            do {
+                std::cout << "current state " << runner->getState() << std::endl;
+                 //current_time += timestep;
+                 runner->evolveToTime(current_time);
+                 std::cout << "subgridfactor " << subfactor << " simulation_time " << current_time << " buildup " << std::endl;
+            } while (!runner->getState().isGridStationary());
+            std::cout << "switched to new mesh width: " << active_meshwidth << std::endl;
+          }
+#endif
+
+          for (double time=0.0; time < endtime; time+=timestep) {
+            double start_time = MPI_Wtime();
+            runner->evolveToTime(time);
+            //runner->gatherCurrentSolution();
+            double stop_time = MPI_Wtime();
+            std::cout << "subgridfactor " << subfactor << " simulation_time " << time << " done " << " computation_time " << stop_time - start_time << std::endl;
+          }
+
+          double stop_meas_time = MPI_Wtime();
+          std::cout << "subgridfactor " << subfactor << " total simulation time " << stop_meas_time - start_meas_time << std::endl;
+          std::cout << std::endl;
+
+#if defined(Parallel)
+      } else {
+        runner->runWorker();
       }
-
-      double stop_meas_time = MPI_Wtime();
-      std::cout << "total simulation time " << stop_meas_time - start_meas_time << std::endl;
-
-#if defined(Parallel)
-  } else {
-    runner->runWorker();
-  }
 #endif
 
-  // experiment done -> cleanup
+      // experiment done -> cleanup
 
-  delete runner;
- 
+      delete runner;
+  }
+
+  peano::shutdownParallelEnvironment();
+  peano::shutdownSharedMemoryEnvironment();
+
   if(_configuration != 0) {
     delete _configuration;
   }
