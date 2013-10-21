db3dbe25e4ee918d65c77ff5c3f2428912f89fb5
Roland Wittmann <wittmanr@in.tum.de>
2013-10-21 07:44:22 +0200

    added peanoclaw specfic load balancer

---
+++ src/peanoclaw/PeanoClawControlLoopLoadBalancer.h
@@ -0,0 +1,48 @@
+#ifndef _DEFAULT_CONTROL_LOOP_LOAD_BALANCER_H_
+#define _DEFAULT_CONTROL_LOOP_LOAD_BALANCER_H_
+
+#include "mpibalancing/ControlLoopLoadBalancer/ControlLoopLoadBalancer.h"
+
+class PeanoClawStrategy : public mpibalancing::ControlLoopLoadBalancer::Strategy {
+    public:
+        PeanoClawStrategy(
+            mpibalancing::ControlLoopLoadBalancer::History<mpibalancing::ControlLoopLoadBalancer::WorkerData>& masterHistory, 
+            mpibalancing::ControlLoopLoadBalancer::HistorySet<int, mpibalancing::ControlLoopLoadBalancer::WorkerData>& workerHistorySet,
+            mpibalancing::ControlLoopLoadBalancer::History<mpibalancing::ControlLoopLoadBalancer::GridStateData>& gridStateHistory
+        );
+
+        virtual ~PeanoClawStrategy();
+ 
+        virtual int run( int worker );
+    private:
+        mpibalancing::ControlLoopLoadBalancer::History<mpibalancing::ControlLoopLoadBalancer::WorkerData>& _masterHistory;
+        mpibalancing::ControlLoopLoadBalancer::HistorySet<int, mpibalancing::ControlLoopLoadBalancer::WorkerData>& _workerHistorySet;
+        mpibalancing::ControlLoopLoadBalancer::History<mpibalancing::ControlLoopLoadBalancer::GridStateData>& _gridStateHistory;
+};
+
+class PeanoClawControlLoopLoadBalancer : public mpibalancing::ControlLoopLoadBalancer::ControlLoopLoadBalancer {
+    public:
+        PeanoClawControlLoopLoadBalancer();
+        virtual ~PeanoClawControlLoopLoadBalancer();
+  
+        virtual mpibalancing::ControlLoopLoadBalancer::HistorySet< int, mpibalancing::ControlLoopLoadBalancer::WorkerData >& getWorkerHistorySet();
+        virtual mpibalancing::ControlLoopLoadBalancer::History<mpibalancing::ControlLoopLoadBalancer::WorkerData>& getMasterHistory();
+        virtual mpibalancing::ControlLoopLoadBalancer::History<mpibalancing::ControlLoopLoadBalancer::GridStateData>& getGridStateHistory();
+
+        virtual mpibalancing::ControlLoopLoadBalancer::Strategy& getStrategy(void);
+        virtual mpibalancing::ControlLoopLoadBalancer::FilterStrategy& getFilterStrategy(void);
+ 
+        void suspendLoadBalancing(bool flag=true);
+    private:
+        bool _loadBalancingSuspended;
+
+        mpibalancing::ControlLoopLoadBalancer::StdHistoryMap< int, mpibalancing::ControlLoopLoadBalancer::WorkerData, mpibalancing::ControlLoopLoadBalancer::FIFOHistory<mpibalancing::ControlLoopLoadBalancer::WorkerData, 2> > _workerHistorySet;
+        mpibalancing::ControlLoopLoadBalancer::FIFOHistory< mpibalancing::ControlLoopLoadBalancer::WorkerData, 2> _masterHistory;
+        mpibalancing::ControlLoopLoadBalancer::FIFOHistory< mpibalancing::ControlLoopLoadBalancer::GridStateData, 2> _gridStateHistory;
+
+        PeanoClawStrategy _strategy;
+        mpibalancing::ControlLoopLoadBalancer::strategies::ContinueStrategy _continueStrategy;
+        mpibalancing::ControlLoopLoadBalancer::BasicFilterStrategy _filterStrategy;
+};
+
+#endif // _DEFAULT_CONTROL_LOOP_LOAD_BALANCER_H_
